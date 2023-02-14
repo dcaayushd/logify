9dbe598e7d70930debed77995276e8d38d217d90
Aayush Dc <aayushdc123@yahoo.com>
2023-02-14 22:01:42 +0545

    login screen created

---
+++ android/app/src/main/kotlin/com/example/logify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.logify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen/login_screen.dart';
+import 'utils/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Logify',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: kPrimaryColor,
+              fontFamily: 'Montserrat',
+            ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
